8f24468bd7d9503e8f2fb0286baad535df6eeaa0
Eduardo Godoy <eduardogodoy.dw@gmail.com>
2023-09-18 20:59:49 -0300

    Terminando el proyecto

--- src/firebase/Client.js
+++ src/firebase/Client.js
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
-export const auth = getAuth()
+export const auth = getAuth(REACT_APP_apiKey)
 export const storage = getStorage()

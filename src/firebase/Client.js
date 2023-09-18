8d91bff0050502b26a666db44385487837c07175
Eduardo Godoy <eduardogodoy.dw@gmail.com>
2023-09-18 20:03:35 -0300

    Terminando el proyecto

--- src/firebase/Client.js
+++ src/firebase/Client.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId:process.env.messagingSenderId,
+  appId: process.env.appId
 }
 
 const app = initializeApp(firebaseConfig)

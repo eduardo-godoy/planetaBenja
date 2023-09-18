7beeee631ffbbde922314ee484a7f2a4a33bddb8
Eduardo Godoy <eduardogodoy.dw@gmail.com>
2023-09-18 20:35:44 -0300

    Terminando el proyecto

--- src/firebase/Client.js
+++ src/firebase/Client.js
@@ -2,16 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  apiKey: process.env.REACT_APP_1,
+  authDomain: process.env.REACT_APP_2,
+  projectId: process.env.REACT_APP_3,
+  storageBucket: process.env.REACT_APP_4,
+  messagingSenderId: process.env.REACT_APP_5,
+  appId: process.env.REACT_APP_6
 }
 
-const app = initializeApp(firebaseConfig, {
-  experimentalForceLongPolling: true, // this line
-  useFetchStreams: false, // and this line
-})
+const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
